6b3e570693e76f36352fdf0e7ce3e37b2e2a0820
Scott Barnes <sgbarnes@protonmail.com>
2021-12-27 15:54:28 -0600

    Cleanup redis implementation

--- backend/server.py
+++ backend/server.py
@@ -1,34 +1,20 @@
 import json
 import time
 
+import eventlet
 from flask import Flask
 from flask_cors import CORS
 from flask_socketio import SocketIO, join_room
-import eventlet
-from engineio.payload import Payload
 
 from redis_image_receiver import RedisImageReceiver
 
-ROOM_TIMEOUT = 60
 
-Payload.max_decode_packets = 500
 eventlet.monkey_patch()
-
 app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
+
 rooms = {}
-
-
-# Sent from cameras, with a new image
-@socketio.on("new-image")
-def get_image(data):
-    # Create room from hostname
-    room = data.get('hostname')
-    rooms[room] = time.time()
-    # Emit to room
-    image = data.get('image')
-    socketio.emit('image', {'room': room, 'data': image}, room=room)
 
 
 @socketio.on('join')
@@ -39,20 +25,21 @@ def on_join(data):
 
 @app.route("/rooms", methods=["GET"])
 def get_rooms():
+    global rooms
     return json.dumps({'rooms': rooms}), 200, {'ContentType': 'application/json'}
 
 
-def listen_for_images():
+def listen_for_images(redis_url: str, redis_port: int):
+    global rooms
     started_listener_thread = False
-    image_receiver = RedisImageReceiver()
+    image_receiver = RedisImageReceiver(redis_url, redis_port)
     while True:
         # Check for new channels
         channel_list = image_receiver.get_all_channels()
         if channel_list != image_receiver.subscribed_channels:
             image_receiver.set_channels(channel_list)
-            print(image_receiver.subscribed_channels)
             if not started_listener_thread:
-                image_receiver.start_listener_thread()  # TODO: Can't call this until we have a subscribed channel
+                image_receiver.start_listener_thread()
         # Check for new messages
         if image_receiver.has_message() and image_receiver.subscribed_channels:
             message = image_receiver.get_message()
@@ -60,10 +47,16 @@ def listen_for_images():
             room = str(message.get("channel"))
             rooms[room] = time.time()
             socketio.emit('image', {'room': room, 'data': image}, room=room)
-            print("Emitted message to: " + room)
         time.sleep(0.01)
 
 
 if __name__ == '__main__':
-    socketio.start_background_task(target=listen_for_images)
+    import argparse
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--redis-url', help='Server address to stream images to', default='localhost')
+    parser.add_argument('--redis-port', help='Server port to stream images to', default=6379)
+    args = parser.parse_args()
+
+    socketio.start_background_task(listen_for_images, args.redis_url, args.redis_port)
     socketio.run(app, host='0.0.0.0')
--- streamer/streamer.py
+++ streamer/streamer.py
@@ -8,7 +8,7 @@ import cv2
 from redis_image_sender import RedisImageSender
 
 
-error = False
+shutdown = False
 
 IMAGE_QUALITY = 70
 CONNECTION_TIMEOUT = 30
@@ -25,7 +25,9 @@ class Streamer:
         self.raw_image_queue = queue.Queue()
         self.ready_image_queue = queue.Queue()
         # Image sending client
-        self.image_sender = RedisImageSender(socket.gethostname(), server_address, server_port)
+        self.server_address = server_address
+        self.server_port = int(server_port)
+        self.image_sender = RedisImageSender(socket.gethostname(), self.server_address, self.server_port)
 
     def _set_video_resolution(self, compression_ratio: float):
         # Capture initial image
@@ -40,50 +42,55 @@ class Streamer:
         image = self.capture_image()
         print('Compressed video resolution: ' + str(image.shape))
 
-    def capture_image(self, init=False):
-        global error
+    def capture_image(self):
+        global shutdown
         ret, frame = self.cam.read()
         if not ret:
-            if init:
-                raise ValueError('Failed to capture image - check camera port value')
-            else:
-                error = True
-                print('Exiting capture thread')
-                exit()
+            shutdown = True
+            raise RuntimeError('Failed to capture image - check camera port value')
         return frame
 
     def run(self):
-        print('Starting image capture')
-        while True:
+        global shutdown
+        print('Starting image capture thread')
+        while not shutdown:
             self.raw_image_queue.put(self.capture_image())
             time.sleep(self.cap_delay)  # Prevents capture from eating cpu time
 
     def encode_images(self):
-        print('Starting encoding')
-        global error
+        global shutdown
+        print('Starting encoding thread')
         encode_params = [int(cv2.IMWRITE_JPEG_QUALITY), IMAGE_QUALITY]
-        while not error:
+        while not shutdown:
             raw_image = self.raw_image_queue.get()
             processed_image_data = (cv2.imencode('.jpeg', raw_image, encode_params)[1]).tobytes()
             self.ready_image_queue.put(processed_image_data)
         print('Exiting encoder thread')
 
     def send_images(self):
-        global error
-        while not error:
-            # Check for new image
+        global shutdown
+        print("Started image sending thread.")
+        while not shutdown:
             image_data = self.ready_image_queue.get()
-            self.image_sender.send_message(image_data)
+            try:
+                self.image_sender.send_message(image_data)
+            except ConnectionError:
+                print("Caught connection error to server, trying to reconnect...")
+                time.sleep(1)
+                self.reconnect()
+
+    def reconnect(self):
+        self.image_sender = RedisImageSender(socket.gethostname(), self.server_address, self.server_port)
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--server-url', help='Server address to stream images to', default='localhost')
-    parser.add_argument('--server-port', help='Server port to stream images to', default=6379)
+    parser.add_argument('--redis-url', help='Server address to stream images to', default='localhost')
+    parser.add_argument('--redis-port', help='Server port to stream images to', default=6379)
     args = parser.parse_args()
     # Setup streamer and start threads
-    streamer = Streamer(args.server_url, args.server_port)
+    streamer = Streamer(args.redis_url, args.redis_port)
     captureThread = Thread(target=streamer.run)
     encoderThread = Thread(target=streamer.encode_images)
     senderThread = Thread(target=streamer.send_images)
