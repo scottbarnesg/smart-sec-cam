0e66b0f27bc7699f69c75a62df54174ca0f657e1
Scott Barnes <sgbarnes@protonmail.com>
2022-01-24 18:54:35 -0600

    Explicitly set mimetype and content_type in video response

--- backend/smart_sec_cam/server/server.py
+++ backend/smart_sec_cam/server/server.py
@@ -41,7 +41,7 @@ def get_video_list():
 @app.route("/video/<file_name>", methods=["GET"])
 def get_video(file_name: str):
     global VIDEO_DIR
-    return send_from_directory(VIDEO_DIR, file_name, as_attachment=True)
+    return send_from_directory(VIDEO_DIR, file_name, as_attachment=True, mimetype='video/mp4', content_type='video/mp4')
 
 
 def listen_for_images(redis_url: str, redis_port: int):
--- frontend/smart-sec-cam/src/components/VideoPlayer.js
+++ frontend/smart-sec-cam/src/components/VideoPlayer.js
@@ -4,7 +4,9 @@ const SERVER_URL = process.env.REACT_APP_API_URL
 const VIDEO_ENDPOINT = "/video/"
 
 export default function VideoPlayer({videoFileName}) {
+    const videoUrl = SERVER_URL + VIDEO_ENDPOINT + videoFileName;
+    console.log(videoUrl);
     return (
-        <ReactPlayer url={SERVER_URL + VIDEO_ENDPOINT + videoFileName} width="100%" height="100%" controls={true} />
+        <ReactPlayer url={videoUrl} width="100%" height="100%" controls={true} />
     );
 }
