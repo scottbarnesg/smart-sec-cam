71bb607facc0115f8f88869fff2d952ab82f0987
Scott Barnes <sgbarnes@protonmail.com>
2022-03-20 18:11:42 -0500

    Sort video list so that most recent is first

--- backend/smart_sec_cam/video/manager.py
+++ backend/smart_sec_cam/video/manager.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from typing import List, Dict
 
 from smart_sec_cam.video.writer import VideoWriter
@@ -11,10 +12,24 @@ class VideoManager:
         self.video_dir = video_dir
 
     def get_video_filenames(self) -> List[str]:
-        return [filename for filename in self._get_all_filenames() if self._is_video_file(filename)]
+        file_names = [filename for filename in self._get_all_filenames() if self._is_video_file(filename)]
+        # Get video timestamps from the filename
+        datetimes_to_filenames = {}
+        for file_name in file_names:
+            try:
+                name = self._remove_file_type(file_name)
+            except ValueError:
+                continue
+            channel, timestamp_iso = name.split(VideoWriter.FILENAME_DELIM)
+            video_timestamp = datetime.fromisoformat(timestamp_iso)
+            datetimes_to_filenames.update({video_timestamp: file_name})
+        # Return sorted list of video files, with most recent video first
+        datetimes = list(datetimes_to_filenames.keys())
+        datetimes.sort(reverse=True)
+        return [datetimes_to_filenames[key] for key in datetimes]
 
     def get_video_filenames_by_date(self) -> Dict[str, List[str]]:
-        all_filenames = self._get_all_filenames()
+        all_filenames = [filename for filename in self._get_all_filenames() if self._is_video_file(filename)]
         filenames_by_date = {}
         for filename in all_filenames:
             try:
