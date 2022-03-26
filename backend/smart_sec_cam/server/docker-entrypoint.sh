70c74fe9c73a6e2d049a4c7775027e3f723138e6
Scott Barnes <sgbarnes@protonmail.com>
2022-03-25 22:10:12 -0500

    Fix redis url in docker-entrypoint.sh

--- backend/smart_sec_cam/server/docker-entrypoint.sh
+++ backend/smart_sec_cam/server/docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 if [ "${API_URL}" ]; then sed -i "s/localhost:8444/${API_URL}/g" /backend/build/static/js/*; fi
 
-python smart_sec_cam/server/server.py --redis-url sec-cam-server --video-dir /backend/data/videos/
+python smart_sec_cam/server/server.py --redis-url redis --video-dir /backend/data/videos/
