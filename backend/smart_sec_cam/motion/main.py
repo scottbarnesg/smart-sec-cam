98522b2803929b0d789c61987c0ebfa5a9bca4ab
Scott Barnes <sgbarnes@protonmail.com>
2022-03-27 18:33:24 -0500

    Make motion detection threshold a cli option in motion cli, add to docker-compose

--- backend/smart_sec_cam/motion/main.py
+++ backend/smart_sec_cam/motion/main.py
@@ -8,7 +8,7 @@ CHANNEL_LIST_INTERVAL = 10
 SLEEP_TIME = 0.01
 
 
-def main(redis_url: str, redis_port: int, video_dir: str):
+def main(redis_url: str, redis_port: int, video_dir: str, motion_threshold: int):
     # Fetch list of channels
     # Subscribe to each channel to get frames
     image_receiver = RedisImageReceiver(redis_url, redis_port)
@@ -17,7 +17,8 @@ def main(redis_url: str, redis_port: int, video_dir: str):
     image_receiver.set_channels(active_channels)
     image_receiver.start_listener_thread()
     # Create and start MotionDetection instance for each channel
-    motion_detectors = {channel: MotionDetector(channel, video_dir=video_dir) for channel in active_channels}
+    motion_detectors = {channel: MotionDetector(channel, motion_threshold=motion_threshold, video_dir=video_dir)
+                        for channel in active_channels}
     for detector in motion_detectors.values():
         detector.run_in_background()
     while True:
@@ -46,10 +47,13 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--redis-url', help='Server address to stream images to', default='localhost')
-    parser.add_argument('--redis-port', help='Server port to stream images to', default=6379)
-    parser.add_argument('--video-dir', help='Directory in which video files are stored', default="data/videos")
+    parser.add_argument('--redis-url', help='Server address to stream images to', type=str, default='localhost')
+    parser.add_argument('--redis-port', help='Server port to stream images to', type=int, default=6379)
+    parser.add_argument('--video-dir', help='Directory in which video files are stored', type=str,
+                        default="data/videos")
+    parser.add_argument('--motion-threshold', help='Threshold of inter-frame difference to trigger motion detection',
+                        type=int, default=10000)
 
     args = parser.parse_args()
 
-    main(args.redis_url, args.redis_port, args.video_dir)
+    main(args.redis_url, args.redis_port, args.video_dir, args.motion_threshold)
