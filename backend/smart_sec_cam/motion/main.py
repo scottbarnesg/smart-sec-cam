58b3728c39074066920ea90d8402da2113b63725
Scott Barnes <sgbarnes@protonmail.com>
2022-04-20 21:50:57 -0500

    Update redis subscription list if active_channels changes

--- backend/smart_sec_cam/motion/main.py
+++ backend/smart_sec_cam/motion/main.py
@@ -34,15 +34,24 @@ def main(redis_url: str, redis_port: int, video_dir: str, motion_threshold: int)
         if time.monotonic() - last_channel_check_time > CHANNEL_LIST_INTERVAL:
             print("Checking for new channels...")
             active_channels = image_receiver.get_all_channels()
+            # Check for new channels
+            new_channels = []
             for channel in active_channels:
                 if channel not in motion_detectors.keys():
                     print(f"Adding channel: {channel}")
+                    new_channels.append(channel)
                     motion_detectors[channel] = MotionDetector(channel, video_dir=video_dir)
+            # Check for removed channels
+            removed_channels = []
             for channel in motion_detectors.keys():
                 if channel not in active_channels:
                     print(f"Removing channel: {channel}")
+                    removed_channels.append(channel)
                     motion_detectors.get(channel).stop()
                     del motion_detectors[channel]
+            # If the active channel list changed, update the redis subscription list
+            if new_channels or removed_channels:
+                image_receiver.set_channels(active_channels)
             last_channel_check_time = time.monotonic()
 
 
