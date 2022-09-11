5400720d005c58c59f247ebede5b47aa30540855
Scott Barnes <scottgbarnes@gwu.edu>
2022-09-11 16:23:27 -0500

    Run camera streamer code as a systemd service (#43)
    * Add base .service file and script to configure it
    
    * Fix working directory in service file
    
    * Fix path to ExecStart and WorkingDirectory
    
    * Add shebang to run.sh to fix running under systemd
    
    Co-authored-by: Scott Barnes <sgbarnes@protonmail.com>

--- backend/smart_sec_cam/streamer/run.sh
+++ backend/smart_sec_cam/streamer/run.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 sudo iwconfig wlan0 power off
 python3 streamer.py --redis-url sec-cam-server
---
+++ scripts/create-streamer-service.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Copy base file to the actual file
+cp smart-sec-cam.base.service smart-sec-cam.service
+# Set current user as user to run service as
+echo "Setting user $USER as streamer service user..."
+sed -i "/^User=/ s/$/$USER\n/" smart-sec-cam.service
+# Use current path to set execution path for service file
+echo "Setting execution path for streamer service..."
+path=$(cd ../ && pwd)
+echo "ExecStart=$path/backend/smart_sec_cam/streamer/run.sh" >> smart-sec-cam.service
+echo "WorkingDirectory=$path/backend/smart_sec_cam/streamer/" >> smart-sec-cam.service
+# Copy service file to systemd directory
+echo "Copying service file to systemd service directory..."
+sudo cp smart-sec-cam.service /etc/systemd/system/
+# Enable and start service
+echo "Enabling and starting service..."
+sudo systemctl enable smart-sec-cam
+sudo systemctl start smart-sec-cam
+echo "Done!"
