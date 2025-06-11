2bd4855afd2baa9c41e6f6018dd3a2829c252422
Scott Barnes <scott.barnes.g@gmail.com>
2025-06-10 21:33:13 -0400

    Upgrade to picamera2 (#62)

--- backend/smart_sec_cam/streamer/camera.py
+++ backend/smart_sec_cam/streamer/camera.py
@@ -1,8 +1,6 @@
 from typing import Tuple
-
 import cv2
 import numpy as np
-
 
 class UsbCamera:
     def __init__(self, usb_port: int = 0, resolution: Tuple[int, int] = (480, 640), jpeg_quality: int = 70,
@@ -20,7 +18,7 @@ class UsbCamera:
             raise RuntimeError('Failed to capture image - check camera port value')
         if self.image_rotation:
             frame = self._rotate_image(frame)
-        processed_image_data = (cv2.imencode('.jpeg', frame, self.encode_params)[1]).tobytes()
+        processed_image_data = cv2.imencode('.jpeg', frame, self.encode_params)[1].tobytes()
         return processed_image_data
 
     def close(self):
@@ -34,7 +32,7 @@ class UsbCamera:
     def _rotate_image(self, frame):
         if self.image_rotation == 90:
             return cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-        elif self.image_rotation == -90 or self.image_rotation == 270:
+        elif self.image_rotation in (-90, 270):
             return cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
         elif self.image_rotation == 180:
             return cv2.rotate(frame, cv2.ROTATE_180)
@@ -44,26 +42,35 @@ class UsbCamera:
 
 
 class RPiCamera:
-    def __init__(self, resolution: Tuple[int, int] = (640, 480), jpeg_quality: int = 70, image_rotation: int = 0):
-        from picamera import PiCamera  # Only import picamera at runtime, since it won't install on other systems
+    def __init__(self, resolution: Tuple[int, int] = (1280, 720), jpeg_quality: int = 70, image_rotation: int = 0):
+        from picamera2 import Picamera2 # Only import picamera at runtime, since it won't install on other systems
 
         self.encode_params = [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality]
-        self.camera = PiCamera()
-        self._set_resolution(resolution)
-        self._set_rotation(image_rotation)
+        self.camera = Picamera2()
+        config = self.camera.create_preview_configuration(main={"size": resolution, "format": "RGB888"})
+        self.camera.configure(config)
+        self.camera.start()
+        self.image_rotation = image_rotation
 
     def capture_image(self):
-        frame = np.empty((self.camera.resolution[1], self.camera.resolution[0], 3), dtype=np.uint8)
-        self.camera.capture(frame, format='bgr', use_video_port=True)
-        processed_image_data = (cv2.imencode('.jpeg', frame, self.encode_params)[1]).tobytes()
+        frame = self.camera.capture_array("main")
+        if self.image_rotation:
+            frame = self._rotate_image(frame)
+        processed_image_data = cv2.imencode('.jpeg', frame, self.encode_params)[1].tobytes()
         return processed_image_data
 
     def close(self):
+        self.camera.stop()
         self.camera.close()
 
-    def _set_resolution(self, resolution: Tuple[int, int]):
-        self.camera.resolution = resolution
-
-    def _set_rotation(self, rotation: int):
-        self.camera.rotation = rotation
+    def _rotate_image(self, frame):
+        if self.image_rotation == 90:
+            return cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+        elif self.image_rotation in (-90, 270):
+            return cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        elif self.image_rotation == 180:
+            return cv2.rotate(frame, cv2.ROTATE_180)
+        else:
+            print(f"Invalid rotation value: {self.image_rotation}")
+            return frame
 
