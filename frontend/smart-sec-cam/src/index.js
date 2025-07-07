ec137848faf5e28f87e5b033bb3f236d8d948e57
Scott Barnes <scott.barnes.g@gmail.com>
2025-07-06 20:57:57 -0400

    Fix UI deprecation issues (#67)
    * Fix ReactDOM.render error
    
    * Bump Dockerfile to version 20
    
    * Clean up comments

--- frontend/smart-sec-cam/src/index.js
+++ frontend/smart-sec-cam/src/index.js
@@ -1,32 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import {
   BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
+import { CookiesProvider } from 'react-cookie'
 import './index.css';
 import App from './App';
 import VideoList from "./pages/VideoList";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="stream" element={<App />} />
-        <Route path="videos" element={<VideoList />} />
-        <Route path="register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <CookiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="stream" element={<App />} />
+          <Route path="videos" element={<VideoList />} />
+          <Route path="register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </CookiesProvider>
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
