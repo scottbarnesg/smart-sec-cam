2e324bf88c9eeb198a0ff71795f3d3207ac0fa2c
Scott Barnes <scott.barnes.g@gmail.com>
2025-09-07 12:24:21 -0400

    Fix UI Crash (#78)
    * Attempted fix for UI crash
    
    * Bump version

---
+++ frontend/smart-sec-cam/src/utils/base64ToBlob.js
@@ -0,0 +1,9 @@
+export function base64ToBlob(b64, mime = 'image/jpeg') {
+  const binary = atob(b64);
+  const len = binary.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i++) {
+    bytes[i] = binary.charCodeAt(i);
+  }
+  return new Blob([bytes], { type: mime });
+}
